39e0b323d243a24d9189ac7fc96f8dbc581de23c
Arda Nakisci <anakisci@gmail.com>
2025-11-07 12:09:00 +0100

    Update Flake to have AWS CLI

--- flake.nix
+++ flake.nix
@@ -5,14 +5,22 @@
     rainix.url = "github:rainlanguage/rainix";
     rain.url = "github:rainlanguage/rain.cli";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "rainix/nixpkgs";
   };
 
-  outputs = { self, flake-utils, rainix, rain }:
+  outputs = { self, flake-utils, rainix, rain, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
-      rec {
+      let
+        pkgs = import nixpkgs { inherit system; };
+        baseDevShells = rainix.devShells.${system};
+        addAwsCli = shell:
+          shell.overrideAttrs (old: {
+            buildInputs = (old.buildInputs or []) ++ [ pkgs.awscli2 ];
+          });
+      in rec {
         packages = rec {
         } // rainix.packages.${system};
 
-        devShells = rainix.devShells.${system};
+        devShells = builtins.mapAttrs (_: addAwsCli) baseDevShells;
       });
 }
